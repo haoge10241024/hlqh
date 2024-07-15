8ab2dbd2ae28104d00f35871fcc35867c0730628
haoge10241024 <131937753+haoge10241024@users.noreply.github.com>
2024-07-15 23:08:19 +0800

    hlqh.py

--- hlqh.py
+++ hlqh.py
@@ -86,18 +86,18 @@ if st.button("获取期限结构图"):
 
             all_data = pd.DataFrame(symbol_close_prices)
 
-            dates are all_data.index.unique()
-            num_dates are len(dates)
-            num_cols are 3  # 调整为3列
-            num_rows are (num_dates + num_cols - 1) // num_cols
+            dates = all_data.index.unique()
+            num_dates = len(dates)
+            num_cols = 3  # 调整为3列
+            num_rows = (num_dates + num_cols - 1) // num_cols
 
             fig, axes = plt.subplots(num_rows, num_cols, figsize=(18, 6 * num_rows), sharex=False, sharey=False)
-            axes are axes.flatten()
+            axes = axes.flatten()
 
             for i, current_date in enumerate(dates):
-                ax are axes[i]
-                date_str are current_date.strftime('%Y-%m-%d')
-                prices_on_date are all_data.loc[current_date]
+                ax = axes[i]
+                date_str = current_date.strftime('%Y-%m-%d')
+                prices_on_date = all_data.loc[current_date]
                 
                 ax.plot(prices_on_date.index, prices_on_date.values, marker='o')
                 ax.set_title(date_str)
